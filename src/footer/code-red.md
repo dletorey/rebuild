bec523399490fc81f198755c9143adcaf8f789e6
Dave Letorey <dave@code-red.uk>
2024-03-16 09:42:56 +0000

    made social links not appear in the footer

--- src/footer/code-red.md
+++ src/footer/code-red.md
@@ -3,4 +3,5 @@ published: true
 name: Code Red Digital
 url: https://code-red.uk
 order: 7
+footer: true
 ---
--- src/footer/footer.11tydata.js
+++ src/footer/footer.11tydata.js
@@ -1,4 +1,5 @@
 module.exports = {
   permalink: false,
-  tags: ["footlink"]
+  tags: ["footlink"],
+  footer: false
 };
