c1063ea0f9de2877ce467c85218ec299f643de55
Dave Letorey <dave@code-red.uk>
2024-05-12 11:56:25 +0200

    created a crad grid

--- src/old-blog/old-blog.11tydata.js
+++ src/old-blog/old-blog.11tydata.js
@@ -1,5 +1,6 @@
 module.exports = {
   layout: "leet",
+  contentType: "old blog",
   tags: ["blog"],
   permalink: "/{{ page.fileSlug }}/index.html",
   redirectFrom: "/blog/{{ page.fileSlug }}/index.html"
--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,5 +1,6 @@
 module.exports = {
   layout: "posts",
+  contentType: "post",
   tags: ["post"],
   eleventyComputed: {
     permalink: data => data.old ? "/blog/{{ page.fileSlug }}/index.html" : "/posts/{{ page.fileSlug }}/index.html",
